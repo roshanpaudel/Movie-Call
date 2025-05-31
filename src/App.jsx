8324e929247ee31add11a09b6fe27135f40ad539
Roshan Paudel <paudelroshan93@gmail.com>
2025-05-31 12:07:14 +1000

    Hero component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Hero } from "./components/Hero";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Hero = () => {
+  return (
+    <div>
+      <nav>
+        <h2>MovieCall</h2>
+      </nav>
+    </div>
+  );
+};
