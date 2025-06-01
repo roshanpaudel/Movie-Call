50de089c9227d9fcbb637875f4eb7df61d2448ae
Roshan Paudel <paudelroshan93@gmail.com>
2025-06-01 23:16:17 +1000

    Display component updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import { Hero } from "./components/Hero";
+import { Display } from "./components/Display";
 
 function App() {
-  
   return (
     <div className="wrapper">
       <Hero />
+      <Display />
     </div>
   );
 }
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
 export const Display = () => {
   return (
-    <div>Display</div>
-  )
-}
+    <div className="container mt-5">
+      <div className="bg-dark">
+        <div className="row">
+          <div className="col">
+            <div class="d-grid gap-2 d-md-block">
+              <button class="btn btn-primary" type="button">
+                All
+              </button>
+              <button class="btn btn-warning" type="button">
+                Drama
+              </button>
+              <button class="btn btn-info" type="button">
+                Action
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
