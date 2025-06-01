e7971d5d67ac33a2bd8d7ae2fade95b7bf34c862
Roshan Paudel <paudelroshan93@gmail.com>
2025-06-01 23:18:45 +1000

    Display component updated with Moviecard

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { MovieCard } from "./MovieCard";
 
 export const Display = () => {
   return (
     <div className="container mt-5">
-      <div className="bg-dark">
+      <div className="bg-dark p-3">
         <div className="row">
           <div className="col">
             <div class="d-grid gap-2 d-md-block">
@@ -17,6 +18,12 @@ export const Display = () => {
                 Action
               </button>
             </div>
+            <div className="mt-3 text-light">2 movies listed</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <MovieCard />
           </div>
         </div>
       </div>
